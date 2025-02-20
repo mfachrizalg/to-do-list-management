f874f0beaf1ebb9f2ea8298e6c1e4b740094e2e8
mfachrizalg <mohammad.fachrizal123@gmail.com>
2025-02-21 00:18:48 +0700

    feat: boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { LikeModule } from './like/like.module';
+import { CommentModule } from './comment/comment.module';
+import { PostModule } from './post/post.module';
+import { TaskModule } from './task/task.module';
+import { ToDoModule } from './to-do/to-do.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    LikeModule,
+    CommentModule,
+    PostModule,
+    TaskModule,
+    ToDoModule,
+  ],
 })
 export class AppModule {}
