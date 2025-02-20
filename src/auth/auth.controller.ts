d26fee7eaa47f56e43f02605f07f68b27407938d
mfachrizalg <mohammad.fachrizal123@gmail.com>
2025-02-21 01:33:05 +0700

    feat: prisma and tables

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login() {}
+
+  @Post('logout')
+  logout() {}
+}
